b35cd7d0f9f999d08c6b5a2553bb61a63f74f412
CharybdeBE <momiamoldava@gmail.com>
2017-03-28 10:17:33 +0200

    THE real GPL LICENCE commit

--- src/groovy/be/cytomine/multidim/hdf5/output/HDF5Geometry.groovy
+++ src/groovy/be/cytomine/multidim/hdf5/output/HDF5Geometry.groovy
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2009-2017. Authors: see NOTICE file.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the GNU Lesser General Public License, Version 2.1 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.gnu.org/licenses/lgpl-2.1.txt
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package be.cytomine.multidim.hdf5.output
 
 /**
