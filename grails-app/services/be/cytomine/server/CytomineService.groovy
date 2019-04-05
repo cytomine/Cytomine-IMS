73864b5b6746fe0ecb8b9a825a72e0f284337fb0
Ulysse Rubens <ulyrubens@hotmail.com>
2019-04-05 15:22:01 +0200

    Rewrite and clean authentication check for upload

--- grails-app/services/be/cytomine/server/CytomineService.groovy
+++ grails-app/services/be/cytomine/server/CytomineService.groovy
@@ -17,6 +17,8 @@ package be.cytomine.server
  */
 
 import be.cytomine.client.Cytomine
+import be.cytomine.client.collections.Collection
+import be.cytomine.client.models.ImageServer
 import be.cytomine.exception.AuthenticationException
 import org.springframework.security.crypto.codec.Base64
 
@@ -32,6 +34,74 @@ class CytomineService {
         String publicKey = grailsApplication.config.cytomine.imageServerPublicKey
         String privateKey = grailsApplication.config.cytomine.imageServerPrivateKey
         return new Cytomine(cytomineUrl, publicKey, privateKey)
+    }
+
+    def getThisImageServer() {
+        def url = grailsApplication.config.cytomine.imageServerURL
+        def servers = Collection.fetch(ImageServer.class)
+        for (int i = 0; i < servers.size(); i++) {
+            def server = servers.get(i)
+            if (server.url == url)
+                return server
+        }
+        return null
+    }
+
+    def getAuthorizationFromRequest(def request) {
+        String authorization = request.getHeader("authorization")
+        if (!authorization
+                || !authorization.startsWith("CYTOMINE")
+                || !authorization.contains(" ")
+                || !authorization.contains(":")) {
+            throw new AuthenticationException("Auth failed: bad authorization")
+        }
+        return [
+                publicKey: authorization.substring(authorization.indexOf(" ") + 1, authorization.indexOf(":")),
+                signature: authorization.substring(authorization.indexOf(":") + 1)
+        ]
+    }
+
+    def getMessageToSignFromRequest(def request) {
+        String contentMD5Header = request.getHeader("content-MD5") ?: ""
+        String dateHeader = request.getHeader("date") ?: (request.getHeader("dateFull") ?: "")
+        if (dateHeader == "") {
+            throw new AuthenticationException("Auth failed: no date")
+        }
+
+        if (request.getHeader("host") == null) {
+            throw new AuthenticationException("Auth failed: no host")
+        }
+
+        String contentTypeHeader = ""
+        if (request.getHeader("content-type")) {
+            contentTypeHeader = request.getHeader("content-type")
+        }
+        if (request.getHeader("Content-Type")) {
+            contentTypeHeader = request.getHeader("Content-Type")
+        }
+        if (request.getHeader("content-type-full")) {
+            contentTypeHeader = request.getHeader("content-type-full")
+        }
+        if (contentTypeHeader == "null") {
+            contentTypeHeader = ""
+        }
+
+        String queryString = (request.getQueryString() != null) ? "?${request.getQueryString()}" : ""
+
+        def messageToSign = "${request.getMethod()}\n$contentMD5Header\n$contentTypeHeader\n$dateHeader\n"
+        messageToSign += "${request.forwardURI}$queryString"
+        return messageToSign
+    }
+
+    def testSignature(def privateKey, def signature, def messageToSign) {
+        SecretKeySpec signingKey = new SecretKeySpec(privateKey.getBytes(), "HmacSHA1")
+        Mac mac = Mac.getInstance("HmacSHA1")
+        mac.init(signingKey)
+        byte[] rawHmac = mac.doFinal(new String(messageToSign.getBytes(), "UTF-8").getBytes())
+        byte[] signatureBytes = Base64.encode(rawHmac)
+        def signatureToTest = new String(signatureBytes)
+
+        return (signature == signatureToTest)
     }
 
     public def tryAPIAuthentification(def cytomineUrl,def ISPubKey, def ISPrivKey, HttpServletRequest request) {
