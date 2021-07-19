c59ce863402f1a135e3dbcbdb7e6251e84ac2580
lrollus <loic.rollus@cytomine.com>
2021-07-19 13:58:13 +0200

    ci jenkins fix

--- scripts/ciPublishWar.sh
+++ scripts/ciPublishWar.sh
@@ -10,7 +10,7 @@ VERSION_NUMBER=$(<"$file")
 
 echo "Build for $VERSION_NUMBER"
 ssh cytomine@192.168.122.16 "mkdir -p /home/cytomine/public_html/drupal7/dwnld/dev/releases/ims/$VERSION_NUMBER"
-scp ./ci/cytomine.war cytomine@192.168.122.16:/home/cytomine/public_html/drupal7/dwnld/dev/releases/ims/$VERSION_NUMBER/IMS.war
+scp ./ci/IMS.war cytomine@192.168.122.16:/home/cytomine/public_html/drupal7/dwnld/dev/releases/ims/$VERSION_NUMBER/IMS.war
 
 IMS_URL="https://cytomine.com/dwnld/dev/releases/ims/$VERSION_NUMBER/IMS.war"
 echo $IMS_URL
